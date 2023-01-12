02b3d2f824d684a9e4012bf4da5290b64a3f03eb
joanajaupi <joanajaupi01@gmail.com>
2023-01-12 18:30:02 +0100

    added client balance ui

---
+++ client/src/controller/balanceController.java
@@ -0,0 +1,28 @@
+package controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+public class balanceController {
+
+    @FXML
+    private Label balanceLbl;
+
+    @FXML
+    private Button closeBtn;
+
+    @FXML
+    private Label idLbl;
+
+    @FXML
+    private Label nameLbl;
+
+    @FXML
+    private Label surnameLbl;
+
+    @FXML
+    void onClose(ActionEvent event) {
+
+    }
+
+}
